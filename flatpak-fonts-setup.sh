b633e3b5f8f000369163f6dc90d0ec28ba2393d1
ShivanshMisra <87258801+ShivanshMisra@users.noreply.github.com>
2023-03-10 11:03:16 +0530

    Update flatpak-fonts-setup.sh

--- flatpak-fonts-setup.sh
+++ flatpak-fonts-setup.sh
@@ -5,20 +5,20 @@
 # Important!
 # You need to have /etc/fonts/local.conf file, an local.conf.example one is included in this directory!
 
-help_string="Purpose: This Will copy your local (/etc/fonts/local.conf) font configuration file so that it can be recognized by the flatpak app. 
-----------------------------------------------------------
-Usage: ./flatpak-fonts-setup.sh [Application-id(of your app)]
-The Application id is included with your application (they can be obtained from the 'flatpak list' command):
-Firefox -> org.mozilla.firefox (for.eg)
-----------------------------------------------------------
-Other Usage: ./flatpak-fonts-setup.sh -h/--help 
-Display this Help Message
-----------------------------------------------------------
-Example:
-To Setup Fonts for Firefox(org.mozilla.firefox):
-./flatpak-fonts-setup.sh org.mozilla.firefox
-- If the output is 'Done!', you are good!
-- If it says 'Please run that application Once and Retry!, Or see if it is even installed!', then do so accordingly.
+help_string="Purpose: This Will copy your local (/etc/fonts/local.conf) font configuration file so that it can be recognized by the flatpak app.\n
+----------------------------------------------------------\n
+Usage: ./flatpak-fonts-setup.sh [Application-id(of your app)]\n
+The Application id is included with your application (they can be obtained from the 'flatpak list' command):\n
+Firefox -> org.mozilla.firefox (for.eg)\n
+----------------------------------------------------------\n
+Other Usage: ./flatpak-fonts-setup.sh -h/--help\n
+Display this Help Message\n
+----------------------------------------------------------\n
+Example:\n
+To Setup Fonts for Firefox(org.mozilla.firefox):\n
+./flatpak-fonts-setup.sh org.mozilla.firefox\n
+- If the output is 'Done!', you are good!\n
+- If it says 'Please run that application Once and Retry!, Or see if it is even installed!', then do so accordingly.\n
 "
 
 # Check if /etc/fonts/local.conf if present
